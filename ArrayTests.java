91fdd3398ea0796fb2dab3721861456e400c2a0c
uloulou <ulou@ucsd.edu>
2024-01-31 13:08:39 -0800

    updated

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,24 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test 
+	public void testReverseInPlace1() {
+    int[] input1 = { 1,2,3 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 3,2,1}, input1);
+	}
+
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversed1() {
+    int[] input1 = {1,3,5,7 };
+    assertArrayEquals(new int[]{ 7,5,3,1}, ArrayExamples.reversed(input1));
+  }
+
 }
